f7d39d6ce0286e23aaeecc12df1e267bf1def785
codebycorey <me@coreyodonnell.com>
2023-07-06 23:41:40 -0400

    add git plugins, update lazy-lock.json

--- lua/vinevim/plugins/git.lua
+++ lua/vinevim/plugins/git.lua
@@ -1,4 +1,38 @@
 return {
-    { "tpope/vim-fugitive", cmd = { "Git", "G" } },
-    { "tpope/vim-rhubarb", cmd = { "Git" }, enable = false },
+    {
+        "tpope/vim-fugitive",
+        cmd = { "Git", "G" },
+    },
+    {
+        "tpope/vim-rhubarb",
+        cmd = { "Git" },
+        enable = false,
+    },
+    {
+        "NeogitOrg/neogit",
+        dependencies = "nvim-lua/plenary.nvim",
+        cmd = { "Neogit" },
+        keys = {
+            {
+                "<leader>g",
+                function()
+                    require("neogit").open()
+                end,
+            },
+        },
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        opts = {
+            signs = {
+                add = { text = "│" },
+                change = { text = "│" },
+                delete = { text = "_" },
+                topdelete = { text = "‾" },
+                changedelete = { text = "~" },
+                untracked = { text = "┆" },
+            },
+        },
+    },
 }
