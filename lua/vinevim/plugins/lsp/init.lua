d723bb0756478b9843d77d08f87548bc04e53b7c
codebycorey <me@coreyodonnell.com>
2025-01-02 13:08:37 -0500

    fix lazydev

--- lua/vinevim/plugins/lsp/init.lua
+++ lua/vinevim/plugins/lsp/init.lua
@@ -100,9 +100,10 @@ return {
         cmd = "LazyDev",
         opts = {
             library = {
-                -- See the configuration section for more details
-                -- Load luvit types when the `vim.uv` word is found
-                -- { path = "luvit-meta/library", words = { "vim%.uv" } },
+                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                { path = "LazyVim", words = { "LazyVim" } },
+                { path = "snacks.nvim", words = { "Snacks" } },
+                { path = "lazy.nvim", words = { "LazyVim" } },
             },
         },
     },
