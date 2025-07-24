15c76394913bc0372dba75fe797fac1831e895b3
codebycorey <me@coreyodonnell.com>
2025-07-24 00:00:35 -0400

    feat(ai): Add codecompanion history, moved to its own file, added mcphub

--- lua/vinevim/plugins/codecompanion.lua
+++ lua/vinevim/plugins/codecompanion.lua
@@ -12,14 +12,21 @@ return {
             "nvim-lua/plenary.nvim",
             "nvim-treesitter/nvim-treesitter",
             "fidget.nvim",
+            "ravitemer/codecompanion-history.nvim",
         },
         opts = {
+            display = {
+                chat = {
+                    show_settings = true,
+                    show_token_count = true,
+                },
+            },
             adapters = {
                 copilot = function()
                     return require("codecompanion.adapters").extend("copilot", {
                         schema = {
                             model = {
-                                default = "claude-sonnet-4",
+                                default = "gpt-4.1",
                             },
                             max_tokens = {
                                 default = 200000,
@@ -47,6 +54,30 @@ return {
                                 provider = "fzf_lua", -- Other options include 'default', 'mini_pick', 'fzf_lua', snacks
                             },
                         },
+                    },
+                },
+            },
+            extensions = {
+                history = {
+                    enabled = true,
+                    opts = {
+                        keymap = "gh",
+                        picker = "fzf_lua",
+                    },
+                },
+                mcphub = {
+                    callback = "mcphub.extensions.codecompanion",
+                    opts = {
+                        -- MCP Tools
+                        make_tools = true, -- Make individual tools (@server__tool) and server groups (@server) from MCP servers
+                        show_server_tools_in_chat = true, -- Show individual tools in chat completion (when make_tools=true)
+                        add_mcp_prefix_to_tool_names = false, -- Add mcp__ prefix (e.g `@mcp__github`, `@mcp__neovim__list_issues`)
+                        show_result_in_chat = true, -- Show tool results directly in chat buffer
+                        format_tool = nil, -- function(tool_name:string, tool: CodeCompanion.Agent.Tool) : string Function to format tool names to show in the chat buffer
+                        -- MCP Resources
+                        make_vars = true, -- Convert MCP resources to #variables for prompts
+                        -- MCP Prompts
+                        make_slash_commands = true, -- Add MCP prompts as /slash commands
                     },
                 },
             },
