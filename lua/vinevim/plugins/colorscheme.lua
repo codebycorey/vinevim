bd39ea6ea675baf387f168d8bbf110847134f481
codebycorey <me@coreyodonnell.com>
2025-08-05 16:33:14 -0400

    feat(colorscheme): use catpuccine

--- lua/vinevim/plugins/colorscheme.lua
+++ lua/vinevim/plugins/colorscheme.lua
@@ -52,7 +52,7 @@ return {
         },
         config = function(_, opts)
             require("catppuccin").setup(opts)
-            -- vim.cmd.colorscheme("catppuccin")
+            vim.cmd.colorscheme("catppuccin")
         end,
     },
     {
@@ -86,7 +86,7 @@ return {
         },
         config = function(_, opts)
             require("kanagawa").setup(opts)
-            vim.cmd.colorscheme("kanagawa")
+            -- vim.cmd.colorscheme("kanagawa")
         end,
     },
     {
